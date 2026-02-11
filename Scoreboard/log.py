2ce8836ff372611a33554622ee8bc5585bc53a08
Schlumf1 <tim.knischewski@gmx.de>
2026-02-11 10:57:32 +0100

    Delete Scoreboard directory

--- Scoreboard/log.py
+++
@@ -1,5 +0,0 @@
-from database import save_log
-
-class Log:
-    def schreiben(self, text):
-        save_log(text)
--- Scoreboard/main.py
+++
@@ -1,7 +0,0 @@
-from hauptmenue import Menue
-from database import init_db
-
-init_db()
-
-menue = Menue()
-menue.starten()
